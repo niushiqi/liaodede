0664f58d9fe05fbea55bb7980481f297b74e8ef4
niushiqi <niushiqi@bytedance.com>
2022-06-20 09:44:52 +0800

    fix: 支持网络请求

--- app/src/main/java/com/dyyj/idd/chatmore/ui/plaza/fragment/PlazaPostFragment.kt
+++ app/src/main/java/com/dyyj/idd/chatmore/ui/plaza/fragment/PlazaPostFragment.kt
@@ -61,7 +61,190 @@ class PlazaPostFragment : BaseFragment<FragmentPlazaPostedBinding, PlazaPostedVi
         return PlazaPostedViewModel()
     }
 
+    override fun lazy    companion object {
+        const val KEY_ID = "ID"
+        const val KEY_TITLE = "KEY_TITLE"
+
+
+        fun create(id: String? = null, topicTile: String? = null): PlazaPostFragment {
+            val fragment = PlazaPostFragment()
+            val bundle = Bundle()
+            bundle.putString(KEY_ID, id)
+            bundle.putString(KEY_TITLE, topicTile)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    var topicID: String? = ""
+    var photoList: ArrayList<Image> = arrayListOf()
+
+    override fun onLayoutId(): Int {
+        return R.layout.fragment_plaza_posted
+    }
+
+    override fun onViewModel(): PlazaPostedViewModel {
+        return PlazaPostedViewModel()
+    }
+
     override fun lazyLoad() {
+        super.lazyLoad()
+        initToobar()
+        initView()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        onCreateEvenBus(this)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        onDestryEvenBus(this)
+        super.onDestroyView()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lazyLoad()
+    }
+
+
+    private fun initToobar() {
+        val layoutToolbar = mBinding.layoutToolbar!!
+        layoutToolbar.toolbarTitleTv.text = "发广场动态"
+        layoutToolbar.toolbarBackIv.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        layoutToolbar.txtSubmit.setOnClickListener {
+            val photoStrings = arrayListOf<String>()
+            photoList?.forEach {
+                if ((it.path.isNotBlank()) and (it.path != "local")) {
+                    photoStrings.add(it.path)
+                }
+            }
+            if (mBinding.etInput.text.isBlank() and (photoStrings.size == 0)) {
+                return@setOnClickListener
+            }
+            showProgressDialog()
+
+            val subscribe = mViewModel.netPublish(topicID, photoStrings, et_input.text.toString()).subscribe({
+                closeProgressDialog()
+                if (it.errorCode == 200) {
+                    Toast.makeText(niceChatContext(), "发布成功", Toast.LENGTH_SHORT).show()
+                    EventBus.getDefault().post(PlazaPostedViewModel.PublishSuccess())
+                    PreferenceUtil.commitBoolean(PlazaCardAdapter.KEY_GUDIE, true)
+                    activity?.finish()
+                } else {
+                    Toast.makeText(niceChatContext(), it.errorMsg, Toast.LENGTH_SHORT).show()
+                }
+            }, {
+                closeProgressDialog()
+                Toast.makeText(niceChatContext(), it.message, Toast.LENGTH_SHORT).show()
+            })
+            mViewModel.mCompositeDisposable.add(subscribe)
+        }
+
+        val toolbar = layoutToolbar.toolbar
+        mActivity.setSupportActionBar(layoutToolbar.toolbar)
+    }
+
+    fun initView() {
+        //选话题
+        topicID = arguments?.getString(KEY_ID)
+        val title = arguments?.getString(KEY_TITLE)
+        if (title != null) {
+            mBinding.tvTopic.text = "#${title}"
+        } else {
+            mBinding.tvTopic.text = "#随便聊聊"
+        }
+//
+//        if (usetID == null) {
+//            mBinding.llTip.visibility = View.VISIBLE
+//        }
+
+        mBinding.tvSelect.setOnClickListener({
+            PlazaTopicsPostedActivity.start(it.context)
+        })
+        //
+        if (photoList?.size ?: 0 < 9) {
+            val image = Image("local", 0, "local", 0)
+            image.type = 1
+            photoList?.add(image)
+        }
+        initSubmit()
+        initRecyclerView(photoList!!)
+
+        mBinding.etInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                initSubmit()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
+    }
+
+    private fun initSubmit() {
+        if ((photoList?.size ?: 0 > 1) or (!mBinding.etInput.text.isNullOrBlank())) {
+            mBinding.layoutToolbar!!.txtSubmit.setBackgroundResource(R.drawable.shape_round_red)
+            mBinding.layoutToolbar!!.txtSubmit.isEnabled = true
+        } else {
+            mBinding.layoutToolbar!!.txtSubmit.setBackgroundResource(R.drawable.shape_round_gray)
+            mBinding.layoutToolbar!!.txtSubmit.isEnabled = false
+        }
+    }
+
+    private fun initRecyclerView(list: java.util.ArrayList<Image>) {
+        mViewModel.getAdapter().initData(list)
+        mBinding.rvList.addOnScrollListener(mViewModel.getAdapter().mScrollListener)
+        mBinding.rvList.layoutManager = GridLayoutManager(context, 3)
+        mBinding.rvList.adapter = mViewModel.getAdapter()
+        mViewModel.getAdapter().setTakePicListener(object : ImageAdapter.ITakePicListener {
+            override fun onTake() {
+                val tempList: ArrayList<Image> = arrayListOf()
+                photoList?.forEach {
+                    if (it.type != 1) {
+                        tempList.add(it)
+                    }
+                }
+                PicSelectActivity.start(context!!, tempList, true)
+            }
+        })
+    }
+
+    @Subscribe
+    fun onSubscribeRefresh(obj: PicPreActivity.RefreshPubPic) {
+        photoList.clear()
+        photoList.addAll(obj.photos)
+
+        if (photoList?.size ?: 0 < 9) {
+            val image = Image("local", 0, "local", 0)
+            image.type = 1
+            photoList?.add(image)
+        }
+        initSubmit()
+        mViewModel.getAdapter().refreshData(photoList!!)
+    }
+
+    @Subscribe
+    fun onPicUploadResult(obj: DataRepository.PicUploadFaile) {
+        Toast.makeText(niceChatContext(), obj.msg, Toast.LENGTH_SHORT).show()
+    }
+
+    @Subscribe
+    fun onSelectTopic(obj: PlazaTopicsPostedAdapter.SelectTopicVM) {
+        topicID = obj.topicID
+        if (obj.title != null) {
+            mBinding.tvTopic.text = "#${obj.title}"
+        }
+    }
+    Load() {
         super.lazyLoad()
         initToobar()
         initView()
--- app/src/main/java/com/dyyj/idd/chatmore/ui/wallet/actiivty/WalletActivity.kt
+++ app/src/main/java/com/dyyj/idd/chatmore/ui/wallet/actiivty/WalletActivity.kt
@@ -21,71 +21,72 @@ import com.dyyj.idd.chatmore.viewmodel.WalletViewModel
  * time   : 2018/05/22
  * desc   :
  */
-class WalletActivity:BaseActivity<ActivityWalletBinding, WalletViewModel>() {
+class WalletActivity : BaseActivity<ActivityWalletBinding, WalletViewModel>() {
 
-  companion object {
+    companion object {
 
-    fun start(context:Context) {
-      val intent = Intent(context, WalletActivity::class.java)
-      context.startActivity(intent)
-    }
-  }
-  override fun onLayoutId(): Int {
-    return R.layout.activity_wallet
-  }
-
-  override fun onViewModel(): WalletViewModel {
-    return WalletViewModel()
-  }
-
-  override fun onToolbar(): Toolbar? {
-    return mBinding.layoutToolbar?.findViewById<Toolbar>(R.id.toolbar)
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    initView()
-    initListener()
-  }
-
-  private fun initView() {
-    mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.text = "提现说明"
-    mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.visibility = View.GONE
-    mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.setOnClickListener {
-      CashNoticeActivity.start(mBinding.root.context)
-    }
-    mBinding.vp.offscreenPageLimit = 0
-    mBinding.vp.adapter = mViewModel.getAdapter(supportFragmentManager)
-    mBinding.tl4.setViewPager(mBinding.vp)
-    mBinding.vp.offscreenPageLimit = 3
-    mBinding.vp.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-      override fun onPageScrollStateChanged(state: Int) {
-      }
-
-      override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-      }
-
-      override fun onPageSelected(position: Int) {
-        if (position == 3) {
-          mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.visibility = View.VISIBLE
-        } else {
-          mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.visibility = View.GONE
+        fun start(context: Context) {
+            val intent = Intent(context, WalletActivity::class.java)
+            context.startActivity(intent)
         }
-        if(position == 0) {
-          EventTrackingUtils.joinPoint(EventBeans("ck_wallet_propertypage",""))
-        } else if (position == 1) {
-          EventTrackingUtils.joinPoint(EventBeans("ck_wallet_coinpage",""))
-        } else if (position == 2) {
-          EventTrackingUtils.joinPoint(EventBeans("ck_wallet_cashpage",""))
-        }//TODO 礼物的埋点
-      }
+    }
 
-    })
-  }
+    override fun onLayoutId(): Int {
+        return R.layout.activity_wallet
+    }
 
-  private fun initListener() {
-    mBinding.layoutToolbar?.findViewById<View>(R.id.toolbar_back_iv)?.setOnClickListener { onBackPressed() }
-    mBinding.layoutToolbar?.findViewById<TextView>(R.id.toolbar_title_tv)?.text = "我的钱包"
+    override fun onViewModel(): WalletViewModel {
+        return WalletViewModel()
+    }
 
-  }
+    override fun onToolbar(): Toolbar? {
+        return mBinding.layoutToolbar?.findViewById<Toolbar>(R.id.toolbar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+        initListener()
+    }
+
+    private fun initView() {
+        mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.text = "提现说明"
+        mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.visibility = View.GONE
+        mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.setOnClickListener {
+            CashNoticeActivity.start(mBinding.root.context)
+        }
+        mBinding.vp.offscreenPageLimit = 0
+        mBinding.vp.adapter = mViewModel.getAdapter(supportFragmentManager)
+        mBinding.tl4.setViewPager(mBinding.vp)
+        mBinding.vp.offscreenPageLimit = 3
+        mBinding.vp.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (position == 3) {
+                    mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.visibility = View.VISIBLE
+                } else {
+                    mBinding.layoutToolbar?.findViewById<TextView>(R.id.right_iv)?.visibility = View.GONE
+                }
+                if (position == 0) {
+                    EventTrackingUtils.joinPoint(EventBeans("ck_wallet_propertypage", ""))
+                } else if (position == 1) {
+                    EventTrackingUtils.joinPoint(EventBeans("ck_wallet_coinpage", ""))
+                } else if (position == 2) {
+                    EventTrackingUtils.joinPoint(EventBeans("ck_wallet_cashpage", ""))
+                }//TODO 礼物的埋点
+            }
+
+        })
+    }
+
+    private fun initListener() {
+        mBinding.layoutToolbar?.findViewById<View>(R.id.toolbar_back_iv)?.setOnClickListener { onBackPressed() }
+        mBinding.layoutToolbar?.findViewById<TextView>(R.id.toolbar_title_tv)?.text = "我的钱包"
+
+    }
 }
